5eb9840a7bebf923858290a7cd57a1b7be5e33a3
Ľubomír Sokolovský <lubo.sokolovsky@gmail.com>
2018-10-26 14:28:59 +0200

    Future code for time when Edge supports the downloads API

--- src/background.js
+++ src/background.js
@@ -1,6 +1,16 @@
 function requestOpenImage(info, tab) {
-    var imgSrc = info.srcUrl;
-    browser.tabs.create({url: imgSrc});
+    var imageUrl = info.srcUrl;
+    browser.tabs.create({url: imageUrl});
+}
+
+function requestSaveImage(info, tab) {
+    var imageUrl = info.srcUrl;
+    var downloading = browser.downloads.download({
+        url: imageUrl,
+        saveAs: true
+    });
+
+    downloading.then();
 }
 
 browser.contextMenus.create({
@@ -9,3 +19,10 @@ browser.contextMenus.create({
     contexts: ["image"],
     onclick: requestOpenImage
 });
+
+browser.contextMenus.create({
+    id: "save-image-extension",
+    title: browser.i18n.getMessage("saveImgAs"),
+    contexts: ["image"],
+    onclick: requestSaveImage
+})
